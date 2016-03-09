1306912815e04f72e1c3eab17c6d7df0ff0d8a50
Julie Koubová <juliekoubova@icloud.com>
2016-03-09 17:33:47 +0100

    multistage build

--- build.js
+++ build.js
@@ -10,51 +10,73 @@ var myth = require('myth');
 var uncss = require('uncss');
 
 const PartialsDir = 'partials';
+const TempDir = 'build-stage1';
+const TempIndex = `${TempDir}/index.html`;
 const DestinationDir = 'build';
-const DestinationIndex = `${DestinationDir}/index.html`;
 
 const UncssOptions = {
-  htmlroot: `${__dirname}/${DestinationDir}`,
+  htmlroot: `${__dirname}/${TempDir}`,
   ignoreSheets: [/\/\/fonts.googleapis.com\/.*/]
 };
 
-fs.readdirSync(PartialsDir).forEach(function (file) {
-  var source = `${__dirname}/${PartialsDir}/${file}`;
-  var contents = fs.readFileSync(source).toString();
 
-  Handlebars.registerPartial(file, contents);
-});
+function loadPartials() {
+  fs.readdirSync(PartialsDir).forEach(function (file) {
+    var source = `${__dirname}/${PartialsDir}/${file}`;
+    var contents = fs.readFileSync(source).toString();
 
-Metalsmith(__dirname)
+    Handlebars.registerPartial(file, contents);
+  });
+}
+
+function buildStage1() {
+  Metalsmith(__dirname)
   .source('src')
-  .destination(DestinationDir)
+  .destination(TempDir)
   .use(inPlace({
     engine: 'handlebars'
   }))
-  .use(metalsmithMyth())
+  .use(metalsmithMyth({ compress: true }))
   .use(htmlMinifier())
-  .use(compress())
   .build(function (err) {
     if (err) throw err;
+    inlineIndexCss();
   });
+}
 
-uncss(
-  [DestinationIndex],
-  _.defaults(UncssOptions, {
-    stylesheets: ['index.css']
-  }),
-  function (error, output) {
-    if (error) throw error;
-    
-    output = myth(output, {compress: true});
-    
-    var indexHtml = fs.readFileSync(DestinationIndex).toString();
-    
-    indexHtml  = indexHtml.replace(
-      /<link[^>]+href=['"]?index.css['"]?[^>]*>/, 
-      '<style>' + output + '</style>'
-    );
-    
-    fs.writeFileSync(DestinationIndex, indexHtml);
-  }
+function inlineIndexCss() {
+  uncss(
+    [TempIndex],
+    _.defaults(UncssOptions, {
+      stylesheets: ['index.css']
+    }),
+    function (error, output) {
+      if (error) throw error;
+      
+      output = myth(output, {compress: true});
+      
+      var indexHtml = fs.readFileSync(TempIndex).toString();
+      
+      indexHtml  = indexHtml.replace(
+        /<link[^>]+href=['"]?index.css['"]?[^>]*>/, 
+        '<style>' + output + '</style>'
+      );
+      
+      fs.writeFileSync(TempIndex, indexHtml);
+      buildStage2();
+    }
 );
+}
+
+function buildStage2() {
+  Metalsmith(__dirname)
+    .source(TempDir)
+    .destination(DestinationDir)
+    .use(compress())
+    .build(function (err) {
+      if (err) throw err;    
+    });
+}
+
+loadPartials();
+buildStage1();  
