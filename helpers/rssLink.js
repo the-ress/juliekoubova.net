f9a4c028cb6ddd402466e92f749fec99e017e143
Julie Koubová <juliekoubova@icloud.com>
2016-04-01 17:46:32 +0200

    link rel=canonical

--- helpers/rssLink.js
+++ helpers/rssLink.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = (ctx) => {
-  var rel = (ctx.data.root.path.href == '/') ? 'alternate' : 'home'; 
+  var rel = (ctx.data.root.isHome) ? 'alternate' : 'home'; 
   return `<link rel="${rel}" ` + 
          ` href="${ctx.data.root.baseUrl}/rss.xml" ` + 
          ` type="application/rss+xml">`;
